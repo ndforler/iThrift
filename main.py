e7121c214c8581d146bba5b87c35b2742e7ef781
icus1 <54911443+icus1@users.noreply.github.com>
2020-07-26 01:02:00 -0400

    searchbar testing

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
 # Import libraries -------------------#
 #you need to properly import requests and BeautifulSoup on to your computer
-from bs4 import BeautifulSoup
-import requests
-import csv
-from django.http import HttpResponse
-
-with open('index.html') as html_file:
-    soup = BeautifulSoup(html_file, 'lxml')
-
-source1 = requests.get('https://ebay.ca/').text
-
-soup = BeautifulSoup(source1, 'lxml')
-
-csv_file = open('cms _scrape', 'w')
-
-csv_writer = cvs.writer(csv_file)
-csv_writer.writerow(['brandname', 'price', 'image'])
-csv_write
-
-for article in soup.find_all('article')
-    brandname = article.div.text
-    print headline
-
-match = soup.titletext
-print(match)
-soup = BeautifulSoup(page.content, 'html.parser')
-
-# we need to figure out how to link the search bar to the search value
-search =
-
-# link the search value into each url in the correct position
-
-ebay = requests.get(ebay_url)
-kijiji = requests.get(kijiji_url)
-grailed = requests.get(grailed_url)
-#testing the url
-
-for span in soup.find_all(class_='amt'):
-    print(span.get_text())
+# from bs4 import BeautifulSoup
+# import requests
+# import csv
+# from django.http import HttpResponse
+#
+# with open('index.html') as html_file:
+#     soup = BeautifulSoup(html_file, 'lxml')
+#
+# source1 = requests.get('https://ebay.ca/').text
+#
+# soup = BeautifulSoup(source1, 'lxml')
+#
+# csv_file = open('cms _scrape', 'w')
+#
+# csv_writer = cvs.writer(csv_file)
+# csv_writer.writerow(['brandname', 'price', 'image'])
+# csv_write
+#
+# for article in soup.find_all('article')
+#     brandname = article.div.text
+#     print headline
+#
+# match = soup.titletext
+# print(match)
+# soup = BeautifulSoup(page.content, 'html.parser')
+#
+# # we need to figure out how to link the search bar to the search value
+# search =
+#
+# # link the search value into each url in the correct position
+#
+# ebay = requests.get(ebay_url)
+# kijiji = requests.get(kijiji_url)
+# grailed = requests.get(grailed_url)
+# #testing the url
+#
+# for span in soup.find_all(class_='amt'):
+#     print(span.get_text())
 
 
 # Import libraries -------------------#
