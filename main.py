85f868163085e6c2f2df7515adccb79478c306cb
icus1 <54911443+icus1@users.noreply.github.com>
2020-07-26 00:40:31 -0400

    python update

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ source1 = requests.get('https://ebay.ca/').text
 
 soup = BeautifulSoup(source1, 'lxml')
 
-csv_file = open('cms_scrape', 'w')
+csv_file = open('cms _scrape', 'w')
 
 csv_writer = cvs.writer(csv_file)
-csv_writer.writerow(['brandname', 'size', 'price', 'image'])
+csv_writer.writerow(['brandname', 'price', 'image'])
 csv_write
 
 for article in soup.find_all('article')
@@ -38,3 +38,48 @@ grailed = requests.get(grailed_url)
 
 for span in soup.find_all(class_='amt'):
     print(span.get_text())
+
+
+# Import libraries -------------------#
+#you need to properly import requests and BeautifulSoup on to your computer
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+page = requests.get("https://www.ebay.ca/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313.TR12.TRC2.A0.H0.Xsocks.TRS0&_nkw=socks&_sacat=0")
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+with open('output.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file)
+
+
+# ebay
+#prints out all of the prices
+# for span in soup.find_all(class_='amt'):
+#     print(span.get_text())
+
+
+# prints out all of the titles
+# for title in soup.find_all(class_='vip'):
+#     print(title.get_text())
+
+# #prints out the image url
+for image in soup.find_all(class_='img'):
+    print(image.get_text())
+    # print(image.span)
+    # print(image.vip)
+    print(image.img)
+    writer.writerow(image.get_text(), image.img)
+
+
+# links = soup.find('figure').find_all('img', src=True)
+# for link in links:
+#     timestamp = time.asctime()
+#     txt = open('%s.jpg' % timestamp, "wb")
+#     link = link["src"].split("src=")[-1]
+#     download_img = urllib2.urlopen(link)
+#     print(txt)
+    # txt.write(download_img.read())
+    #
+    # txt.close()
